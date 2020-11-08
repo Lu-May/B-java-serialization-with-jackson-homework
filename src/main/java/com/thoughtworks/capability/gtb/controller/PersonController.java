c24e473b659db58cdce6d0dc3fc1998cf1780a12
Lu-May <1054998345@qq.com>
2020-11-08 14:29:46 +0800

    [luyuehuan] feat:implement problem 2

--- src/main/java/com/thoughtworks/capability/gtb/controller/PersonController.java
+++ src/main/java/com/thoughtworks/capability/gtb/controller/PersonController.java
@@ -1,9 +1,14 @@
 package com.thoughtworks.capability.gtb.controller;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.thoughtworks.capability.gtb.vo.PersonVo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class PersonController {
--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,8 +1,15 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.IOException;
 
 @Data
 @NoArgsConstructor
@@ -10,7 +17,21 @@ import lombok.NoArgsConstructor;
 public class PersonVo {
 
   private String id;
+  @JsonSerialize(nullsUsing = AgeSerializer.class)
   private Integer age;
   private String name;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private String hobby;
+
+  private class AgeSerializer extends StdSerializer<Integer> {
+    protected AgeSerializer() {
+      super(Integer.class);
+    }
+
+    @Override
+    public void serialize(Integer age, JsonGenerator gen, SerializerProvider provider)
+            throws IOException {
+      gen.writeNumber(0);
+    }
+  }
 }
